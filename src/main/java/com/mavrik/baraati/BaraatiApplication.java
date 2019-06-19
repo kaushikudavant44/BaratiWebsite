08e34e03683a5175e357047156efc4e001db63de
admin <bionischetech@gmail.com>
2019-06-20 00:09:29 +0530

    //Ganesh

---
+++ src/main/java/com/mavrik/baraati/BaraatiApplication.java
@@ -0,0 +1,13 @@
+package com.mavrik.baraati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaraatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaraatiApplication.class, args);
+	}
+
+}
